51e5d03e108ddc976630b06bf6a3672e26adb000
Aldi-H <aldihimawan88@gmail.com>
2023-12-25 12:14:49 +0700

    style: configure custom theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,11 +5,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import theme from "@/theme/theme";
+
 const Providers = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
         {children}
