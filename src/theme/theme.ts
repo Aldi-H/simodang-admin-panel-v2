b423a10d2f28b5b9a0a28e84995f49a3ce319339
Aldi-H <aldihimawan88@gmail.com>
2024-01-10 12:48:13 +0700

    Feat: Add CustomCard component and Card styles to theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,11 +1,19 @@
-import { ThemeConfig, extendTheme } from "@chakra-ui/react";
+import {
+  HTMLChakraProps,
+  ThemeConfig,
+  ThemingProps,
+  extendTheme,
+} from "@chakra-ui/react";
 import { globalStyles } from "./styles";
+import { CardComponent } from "./additions/card/card";
 
 const config: ThemeConfig = {
   initialColorMode: "light",
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config }, globalStyles);
+const theme = extendTheme({ config }, globalStyles, CardComponent);
+
+export interface CustomCardProps extends HTMLChakraProps<"div">, ThemingProps {}
 
 export default theme;
